df9b46547cf71ef1ea4fb837fca3f63ea1232297
Sonologic <gmc@sonologic.nl>
2011-07-20 21:04:25 +0200

    Improve loading and require jquery

--- egtasks.user.js
+++ egtasks.user.js
@@ -4,19 +4,21 @@
 // @description    Extends google tasks canvas implementation
 // @include        https://mail.google.com/tasks/canvas
 // @description    Extends google tasks canvas implementation - Copyright (c) 2011, "Koen Martens" <gmc@sonologic.nl>
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // ==/UserScript==
 
-if(window.document.body.children[0].id=="") {
-  console.log('run');
-
-  //console.log(document.body.childNodes[0].childNodes.length);
 
 
 function wait(check, callback){
   if (check()) callback()
-  else window.setTimeout(wait, 300, check, callback);
+  else window.setTimeout(wait, 100, check, callback);
 }
 
+function egtToggleSubTasks() {
+  alert('toggle');
+}
+
+// the main body of the script
 function execute() {
   console.log('roll');
 
@@ -33,44 +35,44 @@ function execute() {
   var newHeader = idocument.createElement("div");
   newHeader.setAttribute('class','egtasksHeader');
   newHeader.style.height="1.2em";
-  newHeader.innerHTML="egtasks 0.1 activated";
   newHeader.style.background="#B3B9FF";
+  newHeader.style.padding="0 0 0 0.5em";
+
+  newHeader.appendChild(idocument.createTextNode("egtasks 0.1 activated"));
+
+  link1=idocument.createElement('a');
+  link1.setAttribute('href','javascript:egtToggleSubTasks()');
+  link1.appendChild(idocument.createTextNode("hide subtasks"));
+  link1.style.padding="0 0 0 0.5em";
+  
+  newHeader.appendChild(link1);
 
   idocument.body.insertBefore(newHeader,idocument.getElementsByTagName('div')[0]);
 
   idocument.getElementById(':1.lt').style.top="1.22em";
 }
 
-wait(
-  function(){ console.log("iterate"); return document.body.childNodes[0].childNodes.length != 0; },
-  function() {
-    console.log("wait1 done");
-    execute();
-/*    wait(
-      function(){ console.log("iterate"); return document.body.getElementsByTagName("iframe")[0].contentDocument.childNodes.length == 2; },
-      function() { console.log("wait2 done"); execute(); }
-    );*/
-  }
-);
+// only run on canvas itself, not firebug console
+if(window.document.body.children[0].id=="") {
 
+  console.log('run');
 
+	// wait for canvas to be loaded and initialized
+        // (TODO: clean up this mess)
+  wait(
+    function(){
+      console.log("iterate");
+      if(document.body.childNodes[0].childNodes.length!=0) {
+        return document.body.childNodes[0].childNodes[0].contentDocument.getElementById(':1.h');
+      } else {
+        return false;
+      }
+      return document.body.childNodes[0].childNodes.length != 0;
+    },    
+    function() {
+      console.log("wait1 done");
+      execute();    
+    }
+  );
 
-/*
-var idocument = document.body.getElementsByTagName("iframe")[0].contentDocument;
-
-var newCol = idocument.createElement("td");
-
-newCol.setAttribute('class','egtasksWorklist');
-newCol.style.width="20%";
-newCol.innerHTML="work list";
-
-idocument.getElementsByTagName("table")[0].firstChild.firstChild.appendChild(newCol);
-
-var newHeader = idocument.createElement("div");
-newHeader.innerHtml="egtasks 0.1 activated";
-
-idocument.body.insertBefore(newHeader,idocument.body.firstChild);
-
-//document.body.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("table")[0].firstChild;
-*/
 }
